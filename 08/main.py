af55ebf84873df5d06d49f7056b323cbe221a7f4
thomashirz <thomas.hirz@gmx.at>
2019-04-04 17:49:39 +0200

    Mood Checker :)

---
+++ 08/main.py
@@ -0,0 +1,32 @@
+text = "Hi, how is your mood today?"
+
+mood = input("Mood today: ")
+
+
+if mood == "happy":
+    print("It is great to see you happy!")
+
+elif(mood == "nervous"):
+    print("Take a deep breath 3 times.")
+
+elif(mood == "sad"):
+    print("Think positive.")
+
+elif(mood == "excited"):
+    print("Wow, great to hear.")
+
+elif(mood == "relaxed"):
+    print("Chill")
+
+else:
+    print("I don't recognize this mood")
+
+
+
+
+
+
+
+
+
+
