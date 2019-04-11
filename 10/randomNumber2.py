d7230f848dc4b24955ad76c42cc91fe8dc26361a
thomashirz <thomas.hirz@gmx.at>
2019-04-11 18:02:04 +0200

    Random Number with Highscore

---
+++ 10/randomNumber2.py
@@ -0,0 +1,39 @@
+#import random
+
+#details könnten schon ausführlicher erklärt werden "Homework" hat schon 35min begonnen
+
+from random import randint
+
+secret = randint(0, 100)
+
+attempts = 0
+
+with open("score.txt", "r") as score_file:
+    best_score = int(score_file.read())
+    print("Highscore: "+ str(best_score))
+
+score_file.close()
+
+while True:
+
+    attempts += 1
+
+    guess = int(input("Guess the number between 1-100: "))
+
+
+    if secret == guess:
+        if attempts < best_score:
+            with open("score.txt", "w") as score_file:
+                score_file.write(str(attempts))
+        print("Winner, Winner, Chicken Dinner! Here is a cookie")
+        break
+
+    elif secret > guess:
+        print("Oh, you guessed to low. Try again.")
+
+    elif secret < guess:
+        print("You were shooting for the stars, but that's too high. Try again.")
+
+
+print(attempts)
+
---
+++ 10/recap.py
@@ -0,0 +1,14 @@
+#gib alle geraden Zahlen von 1-100 aus.
+
+for item in range(2, 101, 2):
+    print(item)
+
+
+# Check ISBN Number
+
+number = 234491239841283
+oneDigit = number%10
+number/=10
+
+print(oneDigit)
+print(in)
